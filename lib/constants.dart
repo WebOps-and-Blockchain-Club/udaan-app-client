d2a2b609205c16df15ee8b85256bf7baea9a25a5
DIKSHANTAGGARWAL <125082477+DIKSHANTAGGARWAL@users.noreply.github.com>
2024-02-10 17:13:56 +0530

    removed api key

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const String google_api_key = "AIzaSyAUP8QKm1iGrFzDvsD6azvaj5l10t7aNT0";
+const String google_api_key = "";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
